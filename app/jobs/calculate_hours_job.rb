efbf615e7447427356a5c273d6cb1e5f40896e72
Kuba Kuźma <kuba@kubakuzma.com>
2020-03-22 10:09:01 +0100

    fixed scoping

--- app/jobs/calculate_hours_job.rb
+++ app/jobs/calculate_hours_job.rb
@@ -1,23 +1,23 @@
 class CalculateHoursJob < ApplicationJob
-  def perform
+  def perform(user)
     ActiveRecord::Base.transaction do
       Entry
-        .where(exact: false)
+        .where(exact: false, user: user)
         .update_all("hours = EXTRACT(EPOCH FROM ended_at - (SELECT MAX(ended_at) FROM entries e WHERE e.ended_at < entries.ended_at)) / 3600")
       Entry
-        .where(exact: false)
+        .where(exact: false, user: user)
         .where("hours < 0.25")
         .update_all("hours = 0")
       Entry
-        .where(exact: false)
+        .where(exact: false, user: user)
         .where("hours >= 0.25")
         .update_all("hours = round(hours * 2) / 2")
       Entry
-        .where(exact: false)
+        .where(exact: false, user: user)
         .where("hours = (SELECT MAX(hours) FROM entries e WHERE e.day = entries.day)")
         .update_all("hours = 8 - LEAST(8, (SELECT COALESCE(SUM(hours), 0) FROM entries e WHERE entries.day = e.day AND entries.id != e.id))")
       Entry
-        .where(exact: false)
+        .where(exact: false, user: user)
         .where(hours: nil)
         .update_all("hours = 1")
     end
